d648f7bc936e163a48e85fc2b07d6bb1d862e886
Olivier Grisel <olivier.grisel@ensta.org>
2013-10-08 23:33:35 +0200

    work on CLI parser

--- cardice/cli.py
+++ cardice/cli.py
@@ -1,35 +1,100 @@
 import argparse
 import sys
 
+USAGE = """cardice command [options...]"""
 
-def parse_arguments(args):
-    # TODO
-    pass
+
+def make_parser():
+    """Parse commandline arguments using a git-like subcommands scheme"""
+
+    common_parser = argparse.ArgumentParser(
+        add_help=False,
+    )
+    common_parser.add_argument(
+        "--cardice-folder",
+        default="~/.cardice",
+        help="Folder to store the cardice configuration and cluster info."
+    )
+    common_parser.add_argument(
+        "--log-level",
+        default="INFO",
+        help="Minimum log level for the file log (under NXDRIVE_HOME/logs)."
+    )
+    common_parser.add_argument(
+        "--cluster",
+        default=None,
+        help="Perform the command on a specific cluster. "
+             "Otherwise the default cluster is the last selected cluster."
+    )
+    parser = argparse.ArgumentParser(
+        parents=[common_parser],
+        usage=USAGE,
+    )
+
+    subparsers = parser.add_subparsers(
+        title='valid commands',
+    )
+
+    # init a cluster config
+    init_parser = subparsers.add_parser(
+        'init', help='Create a new cluster configuration.',
+        parents=[common_parser],
+        usage="cardice init name",
+    )
+    init_parser.set_defaults(command='init')
+    init_parser.add_argument(
+        "name", help="Name of a new cluster configuration.")
+
+    start_parser = subparsers.add_parser(
+        'start', help="Start the selected cluster configuration.",
+        parents=[common_parser],
+    )
+    start_parser.set_defaults(command='start')
+
+    stop_parser = subparsers.add_parser(
+        'stop', help="Stop the selected cluster configuration.",
+        parents=[common_parser],
+    )
+    stop_parser.set_defaults(command='stop')
+
+    terminate_parser = subparsers.add_parser(
+        'terminate', help="Stop the selected cluster configuration and free"
+                          " all related cloud resources. WARNING: all unsaved"
+                          " data will be lost.",
+        parents=[common_parser],
+    )
+    terminate_parser.set_defaults(command='terminate')
+    return parser
+
 
 class CommandHandler(object):
+    """Dispatch the commandline instructions to the right component."""
 
-    def __init__(self, common_opts):
-        self.common_opts = common_opts
+    def __init__(self, options):
+        self.options = options
+        # TODO: check or initialize the cardice config folder
+        # TODO: configure the logger
 
-
-    def handle(self, cmd, options):
+    def run(self):
         """Execute the specified command parameterized by CLI options"""
-        getattr(self, cmd)(options)
+        getattr(self, 'run_' + self.options.command)
 
     def interrupt(self, cmd, options):
         """Perform clean up operations on user interuptions (if any)"""
-        handler = getattr(self, '' + cmd, None)
+        handler = getattr(self, 'interrupt_' + cmd, None)
         if handler is not None:
             return handler(options)
 
 
 def main(args=None):
     if args is None:
-        args = sys.argv
-    common_opts, cmd, cmd_opts = parse_arguments(args)
+        args = sys.argv[1:]
+
+    parser = make_parser()
+    options = parser.parse_args(args)
     
-    executor = CommandHandler(common_opts)
+    executor = CommandHandler(options)
     try:
-        executor.handle(cmd, cmd_opts)
+        executor.run()
     except KeyboardInterrupt:
-        executor.interupt(cmd, cmd_opts)
+        executor.interupt()
